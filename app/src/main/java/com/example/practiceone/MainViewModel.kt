7eba3e5724a6f62c6ff640a4729fb21e817d5209
pooria-darvishi <pooria.darvishi.82@gmail.com>
2023-03-07 00:26:14 +0330

    add getter ItemList

--- app/src/main/java/com/example/practiceone/MainViewModel.kt
+++ app/src/main/java/com/example/practiceone/MainViewModel.kt
@@ -3,22 +3,26 @@ package com.example.practiceone
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
-    private val itemList = arrayListOf(Color("Blue", "000000", "000000"))
-
+    private val itemList = arrayListOf(Color("Blue", "#000000", "#526760"))
+    fun getItemList(): ArrayList<Color>{
+        return itemList
+    }
     fun addColor(nameColor: String, backgroundColor: String) {
         itemList.add(Color(nameColor, "000000", backgroundColor))
     }
 
-    fun removeColor(color:Color) {
+    fun removeColor(color: Color) {
         itemList.remove(color)
     }
-    fun changeTextColor(textColor: String){
-        for (item in itemList){
+
+    fun changeTextColor(textColor: String) {
+        for (item in itemList) {
             item.textColor = textColor
         }
     }
-    fun resetTextColor(){
-        for (item in itemList){
+
+    fun resetTextColor() {
+        for (item in itemList) {
             item.textColor = "000000"
         }
     }
