77cdf232f9b4dcc911e8bf2b66a398b14905d2a8
pooria-darvishi <pooria.darvishi.82@gmail.com>
2023-03-07 00:18:20 +0330

    Create Adapter

---
+++ app/src/main/java/com/example/practiceone/Adapter.kt
@@ -0,0 +1,34 @@
+package com.example.practiceone
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class Adapter(
+    private val items : ArrayList<Color>
+) :
+    RecyclerView.Adapter<Adapter.ViewHolderAdapter>() {
+    class ViewHolderAdapter(itemView : View): RecyclerView.ViewHolder(itemView){
+        val frameColor: FrameLayout = itemView.findViewById(R.id.frameLayout)
+        val colorName : TextView = itemView.findViewById(R.id.textView)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderAdapter {
+        return ViewHolderAdapter(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_view, parent, false)
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolderAdapter, position: Int) {
+        holder.colorName.text = items[position].nameColor
+        holder.colorName.setTextColor(android.graphics.Color.parseColor(items[position].textColor))
+        holder.frameColor.setBackgroundColor(android.graphics.Color.parseColor(items[position].backgroundColor))
+    }
+}
