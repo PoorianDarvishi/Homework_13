097053569b626c9bc74b045b12bca0fbe37908c2
pooria-darvishi <pooria.darvishi.82@gmail.com>
2023-03-07 00:29:56 +0330

    add recycler in MainActivity

--- app/src/main/java/com/example/practiceone/MainActivity.kt
+++ app/src/main/java/com/example/practiceone/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.practiceone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.practiceone.databinding.ActivityMainBinding
+import java.util.zip.Inflater
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    lateinit var adapter: Adapter
+    private val viewModel : MainViewModel by viewModels()
+    private lateinit var recyclerView: RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        adapter = Adapter(viewModel.getItemList())
+        recyclerView = binding.recyclerView
+        recyclerView.adapter = adapter
     }
 }
