8a2a6ba6f75c265b29a528ed42147ae74a474188
pooria-darvishi <pooria.darvishi.82@gmail.com>
2023-03-08 12:36:44 +0330

    change FrameLayout to ConstraintLayout

--- app/src/main/java/com/example/practiceone/Adapter.kt
+++ app/src/main/java/com/example/practiceone/Adapter.kt
@@ -3,15 +3,15 @@ package com.example.practiceone
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.FrameLayout
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 
 class Adapter(
-    private val items: ArrayList<Color>, private val click: (color: Color) -> Unit
+    private val items: ArrayList<MyColor>, private val click: (myColor: MyColor) -> Unit
 ) : RecyclerView.Adapter<Adapter.ViewHolderAdapter>() {
     class ViewHolderAdapter(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val frameColor: FrameLayout = itemView.findViewById(R.id.frameLayout)
+        val frameColor: ConstraintLayout = itemView.findViewById(R.id.frameLayout)
         val colorName: TextView = itemView.findViewById(R.id.textView)
     }
 
