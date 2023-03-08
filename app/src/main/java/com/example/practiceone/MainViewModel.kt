1bb186337ff8a11a416f896887494eb0d27276d8
pooria-darvishi <pooria.darvishi.82@gmail.com>
2023-03-08 12:38:47 +0330

    add Colors to viewModel

--- app/src/main/java/com/example/practiceone/MainViewModel.kt
+++ app/src/main/java/com/example/practiceone/MainViewModel.kt
@@ -4,19 +4,26 @@ import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
     private val itemList = arrayListOf(
-        Color("Blue", "#000000", "#526760"),
-        Color("Blue", "#000000", "#526760"),
-        Color("Blue", "#000000", "#526760"),
+        MyColor("Blue", "#000000", "#0000FF"),
+        MyColor("Red", "#000000", "#ff0000"),
+        MyColor("Green", "#000000", "#28721D"),
+        MyColor("Aqua", "#000000", "#00FFFF"),
+        MyColor("Teal", "#000000", "#008080"),
+        MyColor("Navy", "#000000", "#000080"),
+        MyColor("Fuchsia", "#000000", "#FF00FF"),
+        MyColor("Purple", "#000000", "#800080"),
+        MyColor("Maroon", "#000000", "#800000"),
+        MyColor("Lime", "#000000", "#00FF00"),
     )
-    fun getItemList(): ArrayList<Color>{
+    fun getItemList(): ArrayList<MyColor>{
         return itemList
     }
     fun addColor(nameColor: String, backgroundColor: String) {
-        itemList.add(Color(nameColor, "000000", backgroundColor))
+        itemList.add(MyColor(nameColor, "000000", backgroundColor))
     }
 
-    fun removeColor(color: Color) {
-        itemList.remove(color)
+    fun removeColor(myColor: MyColor) {
+        itemList.remove(myColor)
     }
 
     fun changeTextsColors(textColor: String) {
@@ -27,7 +34,7 @@ class MainViewModel : ViewModel() {
 
     fun resetTextsColors() {
         for (item in itemList) {
-            item.textColor = "000000"
+            item.textColor = "#000000"
         }
     }
 }
